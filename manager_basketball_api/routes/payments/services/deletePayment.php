fe0ee38b69903997077a742dfd76ce18b7ec25a8
Oscar Serrano <153606230+oscarsxrrano@users.noreply.github.com>
2024-05-18 10:48:35 +0000

    update api payments

---
+++ manager_basketball_api/routes/payments/services/deletePayment.php
@@ -0,0 +1,14 @@
+<?php
+
+function deletePayment($conn, $id) {
+    $q = "DELETE FROM payments WHERE id = '$id'";
+    $result = mysqli_query($conn, $q);
+
+    if ($result) $res = array('status' => 'success');
+    else $res = array('status' => 'error');
+
+    header("Content-Type: application/json");
+    return json_encode($res);
+}
+
+?>
