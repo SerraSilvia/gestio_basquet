6af3fbf963ac8ae7e751b94e79fdd6f629536c65
Anna <anna.elisabet5@gmail.com>
2024-05-20 19:00:48 +0200

    add header

--- web/itb-club/src/components/HeaderComponent.vue
+++ web/itb-club/src/components/HeaderComponent.vue
@@ -1,11 +1,29 @@
 <template>
     <header>
-       <h1>ITBClub</h1> 
+       <IconMenu @click="toggleMenu"/>
+       <h1>ITBClub</h1>
+       <IconLogin @click="clickGoToLogin"/>
     </header>
 </template>
 <script>
+import IconMenu from './icons/IconMenu.vue';
+import IconLogin from './icons/IconLogin.vue';
+
+
 export default {
-    name:'HeaderComponent'
+    name:'HeaderComponent', 
+    components: {
+        IconMenu,
+        IconLogin
+    },
+    methods:{
+        toggleMenu(){
+            console.log("click menu");
+        },
+        clickGoToLogin(){
+            console.log("click go to login");
+        }
+    }
 };
 </script>
 
@@ -14,5 +32,12 @@ export default {
         background: rgb(244,93,69);
         background: linear-gradient(90deg, rgba(244,93,69,1) 0%, rgba(249,77,123,1) 100%);
         padding: 1em;
+        display: flex;
+        justify-content: space-between;
+    }
+    h1{
+        color: white;
+        text-transform: uppercase;
+        font-size: 1.5em;
     }
 </style>
