7141381e123b7197517b161e6905b7dbffbeebc0
SerraSilvia <silvia.serra.martin@gmail.com>
2024-05-21 10:34:39 +0200

    add about-nav

--- web/itb-club/src/components/AboutComponent.vue
+++ web/itb-club/src/components/AboutComponent.vue
@@ -1,5 +1,10 @@
 <template>
     <p>Esto es el about</p>
+    <div class="about-nav">
+        <h5>instal·lacions</h5>
+        <h5>clubs</h5>
+        <h5>tornejos</h5>
+    </div>
 </template>
 <script>
 export default {
