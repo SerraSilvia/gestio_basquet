748ea1acd2b32c6d140d94cd110ccd241fa99074
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-05-21 10:46:29 +0200

    add rows captain + players to teams

--- api/routes/teams/services/createTeam.php
+++ api/routes/teams/services/createTeam.php
@@ -8,8 +8,13 @@ function createTeam($conn) {
     $category = $data['category'];
     $total_score = $data['total_score'];
     $img = $data['img'];
+    $captain = $data['captain'];
+    $player1 = $data['player1'];
+    $player2 = $data['player2'];
+    $player3 = $data['player3'];
+    $player4 = $data['player4'];
 
-    $q = "INSERT INTO teams VALUES (DEFAULT, '$name', '$location_id', '$category', '$total_score', '$img'";
+    $q = "INSERT INTO teams VALUES (DEFAULT, '$name', '$location_id', '$category', '$total_score', '$img', '$captain', '$player1', '$player2', '$player3', '$player4'";
     $result = mysqli_query($conn, $q);
 
     if ($result) $res = array('status' => 'success');
--- api/routes/teams/services/updateTeam.php
+++ api/routes/teams/services/updateTeam.php
@@ -4,12 +4,17 @@ function updateTeam($conn, $id) {
     $data = json_decode(file_get_contents('php://input'), true);
 
     $name = $data['name'];
-    $location_id = $data['addlocation_idress'];
+    $location_id = $data['location_id'];
     $category = $data['category'];
     $total_score = $data['total_score'];
     $img = $data['img'];
+    $captain = $data['captain'];
+    $player1 = $data['player1'];
+    $player2 = $data['player2'];
+    $player3 = $data['player3'];
+    $player4 = $data['player4'];
 
-    $q = "UPDATE teams SET name='$name', location_id='$location_id', category='$category', total_score='$total_score' , img='$img' WHERE id='$id'";
+    $q = "UPDATE teams SET name='$name', location_id='$location_id', category='$category', total_score='$total_score' , img='$img', captain='$captain', player1='$player1', player2='$player2', player3='$player3' , player4='$player4' WHERE id='$id'";
     $result = mysqli_query($conn, $q);
 
     if ($result) {
