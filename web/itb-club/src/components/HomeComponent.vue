b4d46f01159bf41fdfd25638eade86267eba6b67
SerraSilvia <silvia.serra.7e6@itb.cat>
2024-05-21 13:51:32 +0200

    silv- add home

--- web/itb-club/src/components/HomeComponent.vue
+++ web/itb-club/src/components/HomeComponent.vue
@@ -1,5 +1,41 @@
 <template>
-    <p>Esto es el Home</p>
+<div class="home-header">
+    <h4>El nostre esperit</h4>
+    <p> Som una comunitat esportiva vibrant on els jugadors poden desenvolupar les seves habilitats,
+     competir en tornejos i gaudir de l'esport amb diversió independentment del seu nivell.
+    </p>
+</div>
+
+<div class="home-bar">
+      <div class="card">
+        <a href="../">
+          <img src="../assets/img/read.png" alt="" />
+          <h4>Instal·lacions</h4>
+        </a>
+      </div>
+      <div class="card">
+        <a href="../pages/write.php">
+          <img src="../assets/img/edit.png" alt="" />
+          <h4>Clubs</h4>
+        </a>
+      </div>
+      <div class="card">
+        <a href="https://historiasdeterror.com/">
+          <img src="../assets/img/moreTales.jpg" alt="" />
+          <h4>Tornejos</h4>
+        </a>
+      </div>
+    </div>
+
+<div class="classes">
+    <h4>Classes amb els millors</h4>
+    /*ranuras para los profesores*/
+</div>
+
+<div class="">
+</div>
+
+
 </template>
 
 <script>
@@ -9,5 +45,7 @@ export default {
 </script>
 
 <style>
+
+
 
 </style>
