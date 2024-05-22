9e07f5237d4ac7f45effb909d59ea3668a46707b
annacano0 <anna.elisabet5@gmail.com>
2024-05-22 21:16:18 +0200

    change db connection

--- api/db/connection.php
+++ api/db/connection.php
@@ -1,9 +1,8 @@
 <?php
-
 $db_hostname = "localhost";
 $db_username = "root";
-$db_password = "root";
-$db_database = "sports_management";
+$db_password = "";
+$db_database = "itbclub";
 
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 
@@ -11,5 +10,4 @@ if (mysqli_errno($conn)) {
     echo "ERROR al connectar-se amb la base de dades";
     die();
 }
-
 ?>
