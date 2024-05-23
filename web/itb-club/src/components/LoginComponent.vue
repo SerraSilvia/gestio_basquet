680f0654536f35042ace1fe460417c4c36003a9b
annacano0 <anna.elisabet5@gmail.com>
2024-05-23 13:51:53 +0200

    add sessions

--- web/itb-club/src/components/LoginComponent.vue
+++ web/itb-club/src/components/LoginComponent.vue
@@ -1,61 +1,64 @@
 <template>
-     <section>
-        <h2>Log in</h2>
-        <div id="crear-poligon"></div>
-        <form action="doLogin">
+  <section>
+    <h2>Log in</h2>
+    <div id="crear-poligon"></div>
+    <form @submit.prevent="doLogin">
+      <label for="mail">Mail</label>
+      <input type="email" id="mail" v-model="user.email" name="mail" required>
 
-            <label for="mail">Mail</label>
-            <input type="email" id="mail" name="mail">
+      <label for="password">Contrasenya</label>
+      <input type="password" id="password" v-model="user.password" name="password" required>
 
-            <label for="password">Contrasenya</label>
-            <input type="password" id="password" name="password">
-
-            <input class="button" type="submit">
-        </form>
-    </section>
+      <input class="button" type="submit" value="Login">
+    </form>
     <RouterLink to="/register">Registrar-se</RouterLink>
-
+  </section>
 </template>
+
 <script>
 import axios from 'axios';
 
 export default {
-    name:'LoginComponent',
-    data() {
-      return {
-        user: {
-          email: '',
-          password: ''
-        },
-        logedUser:null,
+  name: 'LoginComponent',
+  data() {
+    return {
+      user: {
+        email: '',
+        password: ''
+      },
+      logedUser: null,
+    };
+  },
+  methods: {
+    doLogin() {
+      console.log("Iniciando sesion...");
+      axios.post('auth/login', this.user)
+        .then(response => {
+          console.log('Producto obtenido:', response.data);
+          this.logedUser = response.data;
+          this.clearForm();
+          this.saveSession();
+        })
+        .catch(error => {
+          console.error('Error al intentar hacer el login', error);
+        });
+    },
+    clearForm() {
+      this.user = {
+        email: '',
+        password: ''
       };
     },
-    methods: {//TODO: hay que probar el funcionamiento
-      doLogin() {
-        console.log("Iniciando sesion...");
-        axios.get('auth/login', this.user)
-        .then(response =>{
-          console.log('Producto obtenido:', response.data);
-          this.logedUser=response.data;
-          this.clearForm()
-          this.saveSession()
-        })
-        .catch(error =>{
-          console.error('Error al intentar hacer el login');
-        })
-      },
-      clearForm() {
-        this.user = {
-          email: '',
-          password: ''
-        };
-      },
-      saveSession(){//TODO: falta acabar y que corresponda con los datos obtenidos del json
-        console.log('guardando la sesion en sessionStorage');
-        userData=this.logedUser
-        localStorage.setItem('userData', JSON.stringify(userData));//guardar en el local storage
-      }
+    saveSession() {
+      console.log('Guardando la sesion en sessionStorage');
+      const userData = {
+        name: this.logedUser.name,
+        level: this.logedUser.level,
+        id: this.logedUser.id,
+        location_id: this.logedUser.location_id
+      };
+      sessionStorage.setItem('userData', JSON.stringify(userData));
     }
-  
+  }
 };
 </script>
