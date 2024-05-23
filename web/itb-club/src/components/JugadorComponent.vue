ae0170b2b1ee06b45e463a717c365b0c22de3a58
annacano0 <anna.elisabet5@gmail.com>
2024-05-23 23:36:30 +0200

    add admin buttons

--- web/itb-club/src/components/JugadorComponent.vue
+++ web/itb-club/src/components/JugadorComponent.vue
@@ -1,59 +1,76 @@
 <template>
-    <div class="player-container">
-      <p>{{ playerData ? playerData.name : 'Vacant' }}</p>
-      <div v-if="true" class="admin-teamates">
-        <button class="button" @click="deletePlayer">Eliminar</button>
-      </div>
+  <div class="player-container">
+    <p>{{ playerData ? playerData.name : 'Vacant' }}</p>
+    <div class="admin-teamates">
+      <button 
+        v-if="isAdmin && playerData" 
+        class="button" 
+        @click="deletePlayer"
+      >
+        Eliminar
+      </button>
+      <button 
+        v-if="isAdmin && !playerData" 
+        class="button" 
+        @click="addPlayer"
+      >
+        Afegir
+      </button>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'JugadorComponent',
-    props: {
-      player: {
-        type: Number,
-        required: true
-      }
-    },
-    data() {
-      return {
-        playerData: null,
-      }
-    },
-    methods: {
-      getPlayerData() {
-        axios.get('http://localhost/gestio_basquet/api/routes/people/?id=' + this.player)
-          .then(response => {
-            console.log(response.data);
-            this.playerData = response.data[0];
-          })
-          .catch(error => {
-            console.error('Error al obtener datos del jugador:', error);
-          });
-      }, 
-      deletePlayer(){
+  </div>
+</template>
 
-      }
-    },
-    mounted() {
-      this.getPlayerData();
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'JugadorComponent',
+  props: {
+    player: {
+      type: Number,
+      required: true
     }
-  };
-  </script>
-  
-  <style>
-  .player-container{
-    padding: 1em;
-    border-radius: 1em;
-    background-color: #eff9f6;
-    margin: 0.25em 0em;
-    display: flex;
-    justify-content: space-between;
-
+  },
+  data() {
+    return {
+      playerData: null,
+      isAdmin: false
+    }
+  },
+  methods: {
+    getPlayerData() {
+      axios.get('http://localhost/gestio_basquet/api/routes/people/?id=' + this.player)
+        .then(response => {
+          console.log(response.data);
+          this.playerData = response.data[0];
+        })
+        .catch(error => {
+          console.error('Error al obtener datos del jugador:', error);
+        });
+    },
+    deletePlayer() {
+      // Implementar la lógica para eliminar el jugador
+    },
+    addPlayer() {
+      // Implementar la lógica para añadir el jugador
+    }
+  },
+  mounted() {
+    this.getPlayerData();
+    const userLevel = localStorage.getItem('userLevel');
+    //this.isAdmin = (userLevel === 'admin');
+    this.isAdmin=true;
   }
+};
+</script>
 
-  </style>
-  
+<style>
+.player-container {
+  padding: 1em;
+  border-radius: 1em;
+  background-color: #eff9f6;
+  margin: 0.25em 0em;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
