3448a16b8a7a31c867e2d45579647ed7db813143
annacano0 <anna.elisabet5@gmail.com>
2024-05-24 10:43:03 +0200

    add component code

--- web/itb-club/src/components/UsuariComponent.vue
+++ web/itb-club/src/components/UsuariComponent.vue
@@ -1,29 +1,68 @@
 <template>
-    <p>Esta es la pagina del usuario</p>
-    <div class="user-manager-container">
+    <div id="user-manager-page">
+      <h2>Hola {{ user ? user.name : 'Usuari' }}</h2>
+      <div class="user-manager-container">
         <section>
-            <h2>Les teves dades</h2>
+          <h3>Les teves dades</h3>
+          <button>Modificar</button>
         </section>
-
         <section>
-            <h2>El teu equip</h2>
+          <h3>El teu equip</h3>
+          <EquipComponent class="team-item" v-for="(team, index) in teams" :key="index" :team="team" @selected-team="handleSelectedTeam"></EquipComponent>
         </section>
-
         <section>
-            <h2>Subscripcio</h2>
+          <h3>Subscripcio</h3>
         </section>
-
         <section>
-            <h2>Reserves</h2>
+          <h3>Reserves</h3>
         </section>
-
+        <button>Eliminar compte</button>
+      </div>
     </div>
+  </template>
+  
+  <script>
+import EquipComponent from './EquipComponent.vue';
 
-</template>
-<script>
-//habra que comprobar que el usuario es admin o que su id se corresponde con la id del session storage
-export default {
-    name: 'UsuariComponent'
-};
-</script>
-<style></style>
+  // Habrá que comprobar que el usuario es admin o que su id se corresponde con la id del session storage
+  export default {
+    name: 'UsuariComponent',
+    data() {
+      return {
+        user: null // Inicializa el usuario como null
+      };
+    },
+    methods:{
+        handleSelectedTeam(){
+            console.log("Se administran cosas");
+        }
+    },
+    mounted() {
+      // Aquí podrías agregar lógica para obtener los datos del usuario,
+      // por ejemplo, desde una API o desde sessionStorage
+      const userData = JSON.parse(sessionStorage.getItem('userData'));
+      if (userData) {
+        this.user = userData;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  #user-manager-page{
+    padding: 0em 2em;
+  }
+
+  .user-manager-container {
+    background-color: #eff9f6;
+    margin: 0% 5%;
+    padding: 0.5em 0.75em;
+    border-radius: 1em;
+    color: black;
+  }
+
+  .user-manager-container h3{
+     font-size: 1.25em;   
+  }
+  </style>
+  
