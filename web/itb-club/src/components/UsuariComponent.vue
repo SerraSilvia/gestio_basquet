1f73156df2ac7e9a847a29f15c544468655343ed
annacano0 <anna.elisabet5@gmail.com>
2024-05-24 10:08:17 +0200

    add content to usuari componente

--- web/itb-club/src/components/UsuariComponent.vue
+++ web/itb-club/src/components/UsuariComponent.vue
@@ -1,8 +1,29 @@
 <template>
     <p>Esta es la pagina del usuario</p>
+    <div class="user-manager-container">
+        <section>
+            <h2>Les teves dades</h2>
+        </section>
+
+        <section>
+            <h2>El teu equip</h2>
+        </section>
+
+        <section>
+            <h2>Subscripcio</h2>
+        </section>
+
+        <section>
+            <h2>Reserves</h2>
+        </section>
+
+    </div>
+
 </template>
 <script>
+//habra que comprobar que el usuario es admin o que su id se corresponde con la id del session storage
 export default {
-    name:'UsuariComponent'
+    name: 'UsuariComponent'
 };
 </script>
+<style></style>
