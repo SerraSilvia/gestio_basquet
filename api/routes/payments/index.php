b3b9d88ad3955937bb881a49a0165f8a5c9d34ab
annacano0 <anna.elisabet5@gmail.com>
2024-05-26 16:37:52 +0200

    fix error getPaymentsById.php

--- api/routes/payments/index.php
+++ api/routes/payments/index.php
@@ -7,13 +7,14 @@ include './services/getPaymentById.php';
 include './services/createPayment.php';
 include './services/deletePayment.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && !isset($_REQUEST['id'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && !isset($_REQUEST['id']) && !isset($_REQUEST['user_id'])) {
     echo getPayments($conn); 
 }
-/* TODO: mirar porque da error
-else if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_REQUEST['id'] && !isset($_REQUEST['user_id']))) {
+
+else if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_REQUEST['id']) && !isset($_REQUEST['user_id'])) {
     echo getPaymentById($conn, $_REQUEST['id']);
-}*/
+}
+
 
 else if ($_SERVER['REQUEST_METHOD'] == 'GET' && !isset($_REQUEST['id']) && isset($_REQUEST['user_id'])) {
     echo getPaymentByUserId($conn, $_REQUEST['user_id']);
