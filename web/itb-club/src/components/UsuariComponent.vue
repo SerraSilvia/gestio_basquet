980164f74736dd7c3b27dc21fa06ed7d784b8c4b
annacano0 <anna.elisabet5@gmail.com>
2024-05-26 18:41:16 +0200

    add Reserva, Subscripcions to UsuariComponent

--- web/itb-club/src/components/UsuariComponent.vue
+++ web/itb-club/src/components/UsuariComponent.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="user-manager-page">
-    <h2>Hola {{ user ? user.name : 'Usuari' }}</h2>
+    <h2>Hola, {{ user ? user.name : 'Usuari' }}</h2>
     <div class="user-manager-container">
       <section>
         <h3>Les teves dades</h3>
         <p>{{ user ? user.name : 'carregant...' }} {{ user ? user.surname : 'carregant...' }}</p>
         <p>{{ user ? user.mail : 'carregant...' }}</p>
         <p class="capitalize">{{ user ? user.player_level : 'carregant...' }}</p>
-        <button class="button">Modificar</button>
+        <div class="center-container">
+          <button id="deleteAcount" class="button-pink" @click="deleteAcount">Eliminar compte</button>
+          <button id="modifyAcount" class="button-pink">Modificar dades</button>
+        </div>
       </section>
 
       <section v-if="user && user.level === 'player'">
         <h3>El teu equip</h3>
-        <p v-if="team_id != null">
-          <EquipComponent class="team-item" @selected-team="handleSelectedTeam" :team="team"></EquipComponent>
+        <p v-if="user.team_id != null">
+          <EquipComponent class="center-container team-item" :team="team"></EquipComponent>
         </p>
         <p v-else>Sembla que no tens equip :(</p>
       </section>
 
-      <section>
+      <section v-if="user && user.level === 'player'">
         <h3>Subscripció</h3>
         <PaymentComponent v-for="(payment, index) in payments" :key="index" :payment="payment"></PaymentComponent>
       </section>
@@ -28,8 +31,15 @@
         <ReservaItemComponent v-for="(booking, index) in bookings" :key="index" :booking="booking">
         </ReservaItemComponent>
       </section>
-      <button class="button" @click="logOut">Eliminar compte</button>
+
+      <section v-if="user && user.level === 'player'">
+        <h3>Comentaris</h3>
+        <ComentariComponent v-for="(comment, index) in comments" :key="index" :comment="comment">
+        </ComentariComponent>
+      </section>
+
     </div>
+
   </div>
 </template>
 
@@ -47,6 +57,7 @@ export default {
       team: [],
       bookings: [],
       payments: [],
+      comments:[],
     };
   },
   components: {
@@ -55,33 +66,38 @@ export default {
     PaymentComponent
   },
   methods: {
-    handleSelectedTeam() {
-      console.log("Se administran cosas");
-    },
+
     getBookings() {
-      console.log("Se consiguen reservas");
       this.$axios.get('bookings/?user_id=' + this.user.id)
         .then(response => {
-          console.log("bookings: "+response.data);
           this.bookings = response.data;
         })
         .catch(error => {
           console.error('Error al intentar obtener las reservas', error);
         });
     },
-    getPayments(){//falla la api
+    getPayments() {
       this.$axios.get('payments/?user_id=' + this.user.id)
         .then(response => {
-          console.log("payments: "+response.data);
-          this.bookings = response.data;
+          this.payments = response.data;
         })
         .catch(error => {
           console.error('Error al intentar obtener las reservas', error);
         });
     },
-    logOut() {
+    getTeam() {
+      this.$axios.get('teams/?id=' + this.user.team_id)
+        .then(response => {
+          this.team = response.data[0];
+        })
+        .catch(error => {
+          console.error('Error al intentar obtener el equipo', error);
+        });
+    },
+    deleteAcount() {
       sessionStorage.removeItem("userData");
-      this.$router.push({ path: '/login' });
+      //TODO: aqui se debera eliminar el usuario con la api
+      this.$router.push({ path: '/' });
     }
   },
   mounted() {
@@ -90,6 +106,7 @@ export default {
       this.user = userData;
       this.getBookings();
       this.getPayments();
+      if (this.user.team_id != null) this.getTeam();
     } else {
       this.$router.push({ path: '/login' });
     }
@@ -104,10 +121,11 @@ export default {
 
 .user-manager-container {
   background-color: #eff9f6;
-  margin: 0% 5%;
-  padding: 0.5em 0.75em;
+  margin: 5% 5%;
+  padding: 1.25em 0.75em;
   border-radius: 1em;
   color: black;
+
 }
 
 .user-manager-container h3 {
@@ -115,7 +133,19 @@ export default {
   margin-bottom: 0.5em;
 }
 
-.user-manager-container section{
+.user-manager-container section {
   margin: 1em 0em;
+}
+
+.flex {
+  display: flex;
+}
+
+.center-container {
+  padding: 0.5em 0em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
