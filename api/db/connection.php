200690fe257321b6863f3155ba572653eb049a0d
anna <anna.elisabet5@gmail.com>
2024-05-27 18:06:36 +0200

    fix createTeam.php

--- api/db/connection.php
+++ api/db/connection.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173"); 
+header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 
