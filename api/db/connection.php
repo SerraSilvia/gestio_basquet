27c8a62f6da0bc2f4feec89d080499ff63bfd7e6
annacano0 <anna.elisabet5@gmail.com>
2024-05-27 09:35:26 +0200

    change db connection

--- api/db/connection.php
+++ api/db/connection.php
@@ -2,23 +2,25 @@
 header("Access-Control-Allow-Origin: http://localhost:5173"); 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
-/*
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("HTTP/1.1 200 OK");
     exit;
 }
-
+/*
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(204); 
     exit();
 }*/
 
-$db_hostname = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_database = "itbclub";
+$db_hostname = "sql7.freemysqlhosting.net";
+$db_username = "sql7709574";
+$db_password = "BXimbBLPlM";
+$db_database = "sql7709574";
+$db_port = 3306;
 
-$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
+$conn = new mysqli($db_hostname, $db_username, $db_password, $db_database, $db_port);
+
 
 if (mysqli_errno($conn)) {
     echo "ERROR al connectar-se amb la base de dades";
