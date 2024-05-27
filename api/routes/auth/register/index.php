e134cae271b69a6998f3f62fb6fbba86771dcb3a
anna <anna.elisabet5@gmail.com>
2024-05-27 19:30:33 +0200

    change index.php

--- api/routes/auth/register/index.php
+++ api/routes/auth/register/index.php
@@ -8,24 +8,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $surnames = $data['surnames'];
     $dni = $data['dni'];
     $email = $data['email'];
-    $password = $data['password'];
+    $password = md5($data['password']);
     $birthdate = $data['birthdate'];
     $user_type = $data['user_type'];
     $player_level = $data['player_level'];
 
     $q = "INSERT INTO PEOPLE (name, surnames, dni, email, password, birthdate, user_type, player_level) VALUES ('$name', '$surnames', '$dni', '$email', '$password', '$birthdate', '$user_type', '$player_level')";
-    $result = mysqli_query($conn, $q);
-
-    $res = array();
-    if ($result) {
-        $res['token'] = base64_encode(random_bytes(32));
-        $res['auth_level'] = $user_type;
-    } else {
-        $res = array('status' => 'error');
-    }
-
-    header("Content-Type: application/json");
-    echo json_encode($res);
+    echo $q;
 }
 
 ?>
