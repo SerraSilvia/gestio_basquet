0d21b59d089f691480cda26db5d135c456ae4d0f
anna <anna.elisabet5@gmail.com>
2024-05-27 20:05:30 +0200

    fix db connection

--- api/routes/auth/register/index.php
+++ api/routes/auth/register/index.php
@@ -1,5 +1,5 @@
 <?php
-include "../../db/connection.php";
+include "../../../db/connection.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
@@ -16,8 +16,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $q = "INSERT INTO PEOPLE (name, surnames, dni, email, password, birthdate, user_type, player_level) VALUES ('$name', '$surnames', '$dni', '$email', '$password', '$birthdate', '$user_type', '$player_level')";
     $result = mysqli_query($conn, $q);
 
-    if ($result) $res = array('status' => 'success');
-    else $res = array('status' => 'error');
+    $res = array();
+
+    if ($result){
+        $q = "SELECT * FROM PEOPLE WHERE email = '$email' AND password = '$password'";
+        $result = mysqli_query($conn, $q);
+    
+        if (mysqli_num_rows($result) > 0) {
+    
+            $res = mysqli_fetch_assoc($result);
+        }
+
+    } else $res = array('status' => 'error');
+
+
 
     header("Content-Type: application/json");
     echo json_encode($res);
