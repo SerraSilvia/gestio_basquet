f5790a29b4de036db94c9bde2ccf3693d4ac9792
annacano0 <anna.elisabet5@gmail.com>
2024-05-28 13:53:53 +0200

    add adminUsers

--- web/itb-club/src/components/AdminUsers.vue
+++ web/itb-club/src/components/AdminUsers.vue
@@ -1,15 +1,33 @@
 <template>
     <p>Aqui se mostraran todos los usuarios</p>
+    <UserItemComponent></UserItemComponent>
 </template>
   
 <script>
-
+import UserItemComponent from "./UserItemComponent.vue"
 
 export default {
     name: 'AdminUsers',
     data(){
         return{
-            user:[]
+            user:[],
+            users: []
+        }
+    },
+    components:{
+        UserItemComponent
+    },
+    methods:{   
+        getUsers(){
+            console.log("se consiguen los usuarios");
+            this.$axios.get('people/')
+                .then(response => {
+                    console.log("club: " + response.data[0]);
+                    this.users = response.data[0];
+                })
+                .catch(error => {
+                    console.error('Error al intentar obtener el club', error);
+                });
         }
     },
     mounted(){
@@ -17,13 +35,13 @@ export default {
         if(userData) {
             this.user = userData;
             if(this.user.user_type=="admin"||this.user.user_type=="employee"||this.user.user_type=="professor"){
-                console.log("hola usuario autorizado");
             }else this.$router.push({ path: '/' });
+            this.getUsers();
         } else {
             this.$router.push({ path: '/' });
         }
     }
-}
+};
 
 </script>
   
