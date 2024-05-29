9b18f79d09158ec8ccf20e3b6e98ef9ad9699b32
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-05-29 11:56:30 +0200

    update addPlayer.php

--- api/routes/teams/services/addPlayer.php
+++ api/routes/teams/services/addPlayer.php
@@ -3,18 +3,6 @@
 function addPlayer($conn, $id, $user_id, $player) {
     $data = json_decode(file_get_contents('php://input'), true);
 
-    $name = $data['name'];
-    $location_id = $data['location_id'];
-    $category = $data['category'];
-    $total_score = $data['total_score'];
-    $img = $data['img'];
-    $captain = $data['captain'];
-    $player1 = $data['player1'];
-    $player2 = $data['player2'];
-    $player3 = $data['player3'];
-    $player4 = $data['player4'];
-    $tournament_id = $data['tournament_id'];
-
     $q = "UPDATE TEAMS SET $player='$user_id' WHERE id='$id'";
     $result = mysqli_query($conn, $q);
 
