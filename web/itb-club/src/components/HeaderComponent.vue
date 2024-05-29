a8c869c445a1275f6589de4a2cfb38e858db8bb8
Silvia <silvia.serra.7e6@itb.cat>
2024-05-29 11:39:56 +0200

    silv- fix icon login

--- web/itb-club/src/components/HeaderComponent.vue
+++ web/itb-club/src/components/HeaderComponent.vue
@@ -42,13 +42,14 @@ header{
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
+    right: 0;
     background: rgb(244,93,69);
     background: linear-gradient(90deg, rgba(244,93,69,1) 0%, rgba(249,77,123,1) 100%);
     padding: 1em;
     display: flex;
     justify-content: space-between;
     z-index: 10;
+    align-items: center;
 }
 
 </style>
