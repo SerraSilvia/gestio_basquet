e9308a94666245f2df07af9ee2118ef8444574df
anna <anna.elisabet5@gmail.com>
2024-05-30 19:15:59 +0200

    add functional tournaments

--- web/itb-club/src/components/tournaments/GameItem.vue
+++ web/itb-club/src/components/tournaments/GameItem.vue
@@ -1,10 +1,6 @@
 <template>
-  <div>
+  <div class="game">
     <h3>{{ team1.name }} vs {{ team2.name }}</h3>
-    <div>
-      <p>{{ team1.name }}: {{ game.score_t1 }}</p>
-      <p>{{ team2.name }}: {{ game.score_t2 }}</p>
-    </div>
     <div v-if="displayButtons">
       <p>Selecciona el ganador:</p>
       <button @click="clickTeam1">{{ team1.name }}</button>
@@ -27,18 +23,18 @@ export default {
   },
   data() {
     return {
-      displayButtons: false,
       team1: {},
       team2: {},
       winner: '',
+      winnerId:'',
       user: null,
+      displayButtons: false,
       updatedGame: {}
     };
   },
   methods: {
     async clickTeam1() {
       this.$emit("selected-winner", [1, this.game.team1_id, this.game.tournament_position]);
-      this.displayButtons = false;
 
       this.updatedGame.score_t1 = "15";
       this.updatedGame.score_t2 = "12";
@@ -66,39 +62,50 @@ export default {
         const response1 = await this.$axios.get(`teams/?id=${this.game.team1_id}`);
         this.team1 = response1.data[0];
 
-        //  información del equipo 2
+        // información del equipo 2
         const response2 = await this.$axios.get(`teams/?id=${this.game.team2_id}`);
         this.team2 = response2.data[0];
 
-        if (this.game.score_t1 === 0 && this.game.score_t2 === 0 && this.user && this.user.user_type === "admin") {
-          this.displayButtons = true;
-        } else {
-          this.getWinner();
-        }
+        this.getWinner();
+        this.checkDisplayButtons();
       } catch (error) {
         console.error('Error al obtener información de los equipos:', error);
       }
     },
     getWinner() {
-      if (this.game.score_t1 !== 0) {
+      if (this.game.score_t1 !== 0 || this.game.score_t2 !== 0) {
         if (this.game.score_t1 === this.game.score_t2) {
-          this.winner = this.team1.name;
+          this.winner = this.team1.id;
+          this.winnerId=this.team1.name;
         } else {
           this.winner = this.game.score_t1 > this.game.score_t2 ? this.team1.name : this.team2.name;
+          this.winnerId = this.game.score_t1 > this.game.score_t2 ? this.team1.id : this.team2.id;
         }
+      }
+    },
+    checkDisplayButtons() {
+      if ((this.game.score_t1 === 0 && this.game.score_t2 === 0 || this.game.score_t1 === this.game.score_t2) && this.user && this.user.user_type === 'admin') {
+        this.displayButtons = true;
       }
     }
   },
   async mounted() {
     const userData = JSON.parse(sessionStorage.getItem('userData'));
     this.user = userData;
-    if (userData && userData.user_type === "admin") {
-      this.displayButtons = true;
-      this.updatedGame=this.game;
-    }
+    this.updatedGame = this.game;
     await this.getTeamsInfo();
   }
 };
 </script>
 
-<style></style>
+<style>
+.game {
+  background-color: white;
+  padding: 1em;
+  margin: 2.5% 10%;
+  border-color: aqua;
+  border-width: 3px;
+  border-style: solid;
+  border-radius: 1em;
+}
+</style>
--- web/itb-club/src/components/tournaments/VisualizeTournamentComponent.vue
+++ web/itb-club/src/components/tournaments/VisualizeTournamentComponent.vue
@@ -3,7 +3,7 @@
     <div class="display-flex">
       <h2>{{ tournamentSelected.name }}</h2>
       <div class="location">
-        <IconLocation></IconLocation>
+        <IconLocation />
         <p>{{ tournamentClub ? tournamentClub.name : 'Carregant..' }}</p>
       </div>
     </div>
@@ -12,20 +12,23 @@
       <p class="resaltat">{{ tournamentSelected.date_start }} / {{ tournamentSelected.date_end }}</p>
     </div>
     <div v-if="teams.length > 0" class="tournament-teams">
-      <EquipComponent class="team-item" v-for="(team, index) in teams" :key="index" :team="team"></EquipComponent>
+      <EquipComponent class="team-item" v-for="(team, index) in teams" :key="index" :team="team" />
     </div>
     <div v-else>
       <p class="no-teams-message">No hi ha equips inscrits</p>
     </div>
-    <button @click="addMyTeam"
-      v-if="user.user_type != 'admin' && teams.length < 8 && status == 'Inscripcions Obertes'">Incriure el meu
-      equip</button>
-    <button @click="createGames" v-if="user.user_type == 'admin' && teams.length == 8">Crear propers partits</button>
+    <button @click="addMyTeam" v-if="user.user_type != 'admin' && teams.length < 8 && status == 'Inscripcions Obertes'">
+      Incriure el meu equip
+    </button>
+    <button @click="createGames" v-if="user.user_type == 'admin' && teams.length == 8 && status!='Finalitzat'">
+      Crear propers partits
+    </button>
+    <p v-else >{{ winner }}</p>
   </div>
   <div v-if="tournamentSelected && games.length > 0">
     <h2>Partits</h2>
     <div class="game-container">
-      <GameItem v-for="(game, index) in games" :key="game.id" :game="game" @selected-winner="handleWinner"></GameItem>
+      <GameItem v-for="(game, index) in games" :key="game.id" :game="game" @selected-winner="handleWinner" :class="{'special-team': index === 6}" />
     </div>
   </div>
 </template>
@@ -41,7 +44,7 @@ export default {
     id: {
       type: String,
       required: true,
-    }
+    },
   },
   data() {
     return {
@@ -52,17 +55,18 @@ export default {
       user: {}, // Cambiado a objeto
       maxRound: 0,
       games: [],
+      winner:[],
     };
   },
   components: {
     IconLocation,
     EquipComponent,
-    GameItem
+    GameItem,
   },
   methods: {
     async getTournament() {
       try {
-        const response = await this.$axios.get(`tournaments/?id=` + this.id);
+        const response = await this.$axios.get(`tournaments/?id=${this.id}`);
         this.tournamentSelected = response.data[0];
         await this.getClub();
         await this.getTeams();
@@ -77,7 +81,7 @@ export default {
         return;
       }
       try {
-        const response = await this.$axios.get(`locations/?id=` + this.tournamentSelected.location_id);
+        const response = await this.$axios.get(`locations/?id=${this.tournamentSelected.location_id}`);
         this.tournamentClub = response.data[0];
       } catch (error) {
         console.error('Error al intentar obtener el club', error);
@@ -98,115 +102,139 @@ export default {
         this.status = "Finalitzat";
       }
     },
-    async getTeams() {
-      try {
-        const response = await this.$axios.get(`teams/?tournament_id=` + this.id);
-        this.teams = response.data;
-      } catch (error) {
-        console.error('Error al intentar obtener los equipos del club', error);
-      }
+    getTeams() {
+      this.$axios
+        .get(`teams/?tournament_id=${this.id}`)
+        .then((response) => {
+          this.teams = response.data;
+        })
+        .catch((error) => {
+          console.error('Error al intentar obtener los equipos del club', error);
+        });
     },
-    async addMyTeam() {
-      console.log("añadiendo equipo al torneo");
-      try {
-        const response = await this.$axios.get(`teams/?id=` + this.user.team_id);
-        const updateTeam = response.data[0];
-        updateTeam.tournament_id = this.tournamentSelected.id;
-        await this.$axios.put(`teams/?id=${updateTeam.id}`, updateTeam);
-        await this.getTeams();
-        this.getStatus();
-      } catch (error) {
-        console.error('Error al intentar modificar el equipo', error);
-      }
+    addMyTeam() {
+      this.$axios
+        .get(`teams/?id=${this.user.team_id}`)
+        .then((response) => {
+          const updateTeam = response.data[0];
+          updateTeam.tournament_id = this.tournamentSelected.id;
+          return this.$axios.put(`teams/?id=${updateTeam.id}`, updateTeam);
+        })
+        .then(() => {
+          this.getTeams();
+          this.getStatus();
+        })
+        .catch((error) => {
+          console.error('Error al intentar modificar el equipo', error);
+        });
     },
-    async createGames() {
-      try {
-        const response = await this.$axios.get(`games/?tournament_id=` + this.tournamentSelected.id);
-        this.games = response.data;
-        this.maxRound = this.games.length;
-      } catch (error) {
-        console.error('Error al obtener los partidos del torneo', error);
-      }
+    createGames() {
+      this.$axios
+        .get(`games/?tournament_id=${this.tournamentSelected.id}`)
+        .then((response) => {
+          this.games = response.data;
+          this.maxRound = this.games.length;
 
-      console.log("se crean los juegos");
-      if (this.maxRound < 4) {
-        console.log("primera ronda");
-        this.gameInsert(this.teams[0], this.teams[1], 0);
-        this.gameInsert(this.teams[2], this.teams[3], 1);
-        this.gameInsert(this.teams[4], this.teams[5], 2);
-        this.gameInsert(this.teams[6], this.teams[7], 3);
-      } else if (this.maxRound < 6) {
-        console.log("segunda ronda");
-        // this.gameInsert(this.getWinner(0), this.getWinner(1), 4);
-        // this.gameInsert(this.getWinner(2), this.getWinner(3), 5);
-      } else {
-        console.log("tercera ronda");
-        //this.gameInsert(this.getWinner(4), this.getWinner(5), 6);
-      }
+          if (this.maxRound < 4) {
+            console.log("primera ronda");
+            this.gameInsert(this.teams[0].id, this.teams[1].id, 0);
+            this.gameInsert(this.teams[2].id, this.teams[3].id, 1);
+            this.gameInsert(this.teams[4].id, this.teams[5].id, 2);
+            this.gameInsert(this.teams[6].id, this.teams[7].id, 3);
+            this.getGames();
+          } else if (this.maxRound >= 4 && this.maxRound < 6) {
+            console.log("segunda ronda");
+            this.gameInsert(this.getWinner(0), this.getWinner(1), 4);
+            this.gameInsert(this.getWinner(2), this.getWinner(3), 5);
+            this.getGames();
+          } else if (this.maxRound == 6){
+            console.log("tercera ronda");
+            this.gameInsert(this.getWinner(4), this.getWinner(5), 6);
+            this.getGames();
+          }else{
+            //el juego ha acabado
+            this.status="Finalitzat";
+          }
+        })
+        .catch((error) => {
+          console.error('Error al obtener los partidos del torneo', error);
+        });
     },
-    async gameInsert(team1, team2, gamePos) {
-      console.log("se crean los inserts");
+    gameInsert(team1, team2, gamePos) {
+      console.log("se crean los inserts: "+team1+" "+team2);
       const newGame = {
         id_booking: 54,
         tournament_id: this.tournamentSelected.id,
-        team1_id: team1.id,
-        team2_id: team2.id,
+        team1_id: team1,
+        team2_id: team2,
         score_t1: 0,
         score_t2: 0,
-        tournament_position: gamePos
+        tournament_position: gamePos,
       };
-      try {
-        const response = await this.$axios.post("games/", newGame);
-        if (response.data.status !== "error") {
-          console.log("Partido creado con exito");
-        } else {
-          console.error("Error en la creación del partido:", response.data.message);
-        }
-      } catch (error) {
-        console.error("Error al agregar el partido:", error);
-      }
-    },
-    async getGames() {
-      try {
-        const response = await this.$axios.get("games/?tournament_id=" + this.tournamentSelected.id);
-        if (response.data.status !== "error") {
-          this.games = response.data;
-        } else {
-          console.error("Error al obtener partidos ", response.data.message);
-        }
-      } catch (error) {
-        console.error("Error al obtener partidos:", error);
-      }
+
+      console.log(newGame);
+
+      this.$axios.post("games/", newGame)
+        .then((response) => {
+          if (response.data.status !== "error") {
+            console.log("Partido creado con exito");
+          }
+        })
+        .catch((error) => {
+          console.error("Error al agregar el partido:", error);
+        });
     },
     getWinner(gamePos) {
-      const game = this.games.find(g => g.tournament_position === gamePos);
-      if (!game) return null;
-      return game.score_t1 >= game.score_t2 ? game.team1 : game.team2;
+      const selectedGame = this.games.find((game) => game.tournament_position == (gamePos.toString()));
+      console.log("selected game: "+selectedGame);
+      if (selectedGame) {
+        if (selectedGame.score_t1 >= selectedGame.score_t2) {
+          console.log("get winner:"+selectedGame.team1_id);
+          return (selectedGame.team1_id).toString();
+        } else {
+          console.log("get winner:"+selectedGame.team2_id);
+          return (selectedGame.team2_id).toString();
+        }
+      }
+      return null;
+    },
+    getThisTeam(id) {
+      console.log("se recuperan los datos de: " + id);
+      return this.$axios.get(`teams/?id=${id}`).then((response) => {
+        return response.data[0];
+      });
     },
     handleWinner(data) {
-      console.log("Ganador num: " + data[0] + "Id ganador "+data[1]+" partido: " + data[2]);
-
-      // Obtener los detalles del equipo ganador
-      this.$axios.get(`teams/?id=` + data[1])
-        .then(response => {
+      this.$axios
+        .get(`teams/?id=${data[1]}`)
+        .then((response) => {
           const updateTeam = response.data[0];
-          const newPoints = (Number(updateTeam.total_score))+10;
+          const newPoints = Number(updateTeam.total_score) + 10;
           updateTeam.total_score = newPoints;
-          this.getTeams();
-          return this.$axios.put("teams/?id=" + data[1], updateTeam);
+          return this.$axios.put(`teams/?id=${data[1]}`, updateTeam);
         })
-        .then(updateResponse => {
+        .then((updateResponse) => {
           if (updateResponse.data.status !== "error") {
             console.log("Puntuación del equipo actualizada");
+            this.getTeams();
           } else {
             console.error("Error al actualizar la puntuación del equipo: ", updateResponse.data.message);
           }
         })
-        .catch(error => {
+        .catch((error) => {
           console.error("Error al procesar la actualización de la puntuación del equipo: ", error);
         });
-    }
-
+    },
+    getGames() {
+      this.$axios
+        .get(`games/?tournament_id=${this.id}`)
+        .then((response) => {
+          this.games = response.data;
+        })
+        .catch((error) => {
+          console.error('Error al intentar obtener los partidos del torneo', error);
+        });
+    },
   },
   mounted() {
     const userData = JSON.parse(sessionStorage.getItem('userData'));
@@ -216,7 +244,7 @@ export default {
       this.$router.push({ path: '/login' });
     }
     this.getTournament();
-  }
+  },
 };
 </script>
 
@@ -268,5 +296,10 @@ export default {
   color: white;
   text-align: center;
   padding: 1em;
+}
+
+.special-team{
+  border-color: rgb(106, 255, 83);
+  border-width: 0.5em;
 }
 </style>
