9ec9ce4710ffa79c455096bf57a8135e7939afbd
anna <anna.elisabet5@gmail.com>
2024-05-30 22:45:26 +0200

    remove comment

--- api/routes/teams/services/createTeam.php
+++ api/routes/teams/services/createTeam.php
@@ -8,9 +8,7 @@ function createTeam($conn) {
     $category = $data['category'];
     $img = $data['img'];
     $captain = $data['captain'];
-
-    //INSERT INTO TEAMS (name, location_id, category, total_score, img, captain) VALUES ('name', 1, 'senior', 0, 'prueba.img', '220')
-
+    
     $q = "INSERT INTO TEAMS (name, location_id, category, total_score, img, captain ) VALUES ('$name', $location_id, '$category', 0 , '$img', $captain)";
     $result = mysqli_query($conn, $q);
 
