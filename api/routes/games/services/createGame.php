c1ad0853ecf39a098971315fdee8fd7adc35f2fe
anna <anna.elisabet5@gmail.com>
2024-05-30 22:55:33 +0200

    change api

--- api/routes/games/services/createGame.php
+++ api/routes/games/services/createGame.php
@@ -8,9 +8,9 @@ function createGame($conn) {
     $team2_id = $data['team2_id'];
     $score_t1 = $data['score_t1'];
     $score_t2 = $data['score_t2'];
-    $tourtament_position = $data['tournament_position'];
+    $tournament_position = $data['tournament_position'];
 
-    $q = "INSERT INTO GAMES (id_booking, tournament_id, team1_id, team2_id, score_t1, score_t2, tournament_position) VALUES ($id_booking, $tournament_id, $team1_id, $team2_id, $score_t1, $score_t2, $tourtament_position)";
+    $q = "INSERT INTO GAMES (id_booking, tournament_id, team1_id, team2_id, score_t1, score_t2, tournament_position) VALUES ($id_booking, $tournament_id, $team1_id, $team2_id, $score_t1, $score_t2, $tournament_position)";
     echo $q;
     $result = mysqli_query($conn, $q);
 
